441d5224970cb142840eb70da73b0a7419ee3ae4
Romina.Ayala <romina.ayala@tiendanube.com>
2019-11-29 15:40:52 -0300

    segundo test

---
+++ src aprobadoSuspendido/ejercicioAprobadoSuspendido.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ejercicioAprobadoSuspendido {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Escribe una nota: ");
+
+    int nota = Integer.parseInt(br.readLine());
+
+    if ( nota >=0 && nota <= 10 ) {
+
+      //nota correcta
+      switch ( nota ) {
+        case 5:
+          System.out.println("Aprobado");
+          break;
+        case 7:
+          System.out.println("Notable");
+          break;
+        case 10:
+          System.out.println("Excelente");
+          break;
+
+        default:
+            System.out.println("ERROR");
+            break;
+
+      }
+
+    }
+  // else
+      //System.out.println("El valor es incorrecto");
+
+
+
+  }
+
+}
---
+++ src ejemplovariables/ejemploVariables/EjemploVariables.java
@@ -0,0 +1,14 @@
+package ejemploVariables;
+
+public class EjemploVariables {
+
+  public static void main(String[] args) {
+
+     int a;
+     int b = 0;
+
+     a = 5;
+     b = 7;
+  }
+
+}
